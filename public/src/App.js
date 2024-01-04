495b72198ffed22bad77f3f5cfad9a79ad9fdef1
Utkarsh <utkarshfelix@gmail.com>
2024-01-04 20:50:07 +0530

    Improved Performance

--- public/src/App.js
+++ public/src/App.js
@@ -1,18 +1,21 @@
-import React from "react";
+import React, {lazy,Suspense} from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import SetAvatar from "./components/SetAvatar";
-import Chat from "./pages/Chat";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
+const Register = lazy(() => import('./pages/Register'));
+const Login = lazy(() => import('./pages/Login'));
+const SetAvatar = lazy(() => import('./components/SetAvatar'));
+const Chat = lazy(() => import('./pages/Chat'));
+
 export default function App() {
   return (
     <BrowserRouter>
+    <Suspense fallback={<> Loading... </>}>
       <Routes>
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/setAvatar" element={<SetAvatar />} />
         <Route path="/" element={<Chat />} />
       </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 }
